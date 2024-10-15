0dc363b9dec8e9a8cabb54dc03e9253c60c3e1bb
KarryleJS-practice <karrylelagrimas6@gmail.com>
2024-10-16 07:32:42 +0800

    arrayActivity.js

---
+++ arrayActivity.js
@@ -0,0 +1,17 @@
+// 1. Create an array called favSingers and store your favorite singers
+let favSingers = ["Sarah Geronimo", "Lea Salonga", "Moira Dela Torre"];
+
+// 2. Log the first singer in the array using array index notation
+console.log(favSingers[0]); // Outputs: Singer1
+
+// 3. Create an array called favNumbers and store your four favorite numbers
+let favNumbers = [7, 10, 20, 04];
+
+// 4. Create an array called mixedArr with different data types
+let mixedArr = ["string", ["otherArray"], 123, true];
+
+// 5. Access and log each item in the mixedArr using array index notation
+console.log(mixedArr[0]); // Outputs: string
+console.log(mixedArr[1]); // Outputs: ["otherArray"]
+console.log(mixedArr[2]); // Outputs: 123
+console.log(mixedArr[3]); // Outputs: true
